1d67b09207f6186fcddc6ee3314fcecb19b22376
Matthew McPherrin <git@mcpherrin.ca>
2014-10-30 17:44:06 -0700

    the comment links are pretty useless

--- reddit.py
+++ reddit.py
@@ -23,16 +23,18 @@ STATEFILE="/home/ircbot/state/reddit-%s-%s-storyids"%(CHANNEL,REDDIT)
 sf = open(STATEFILE)
 seen = set(sf.read().split("\n"))
 sf.close()
+print "Previously seen %d posts"%len(seen)
 
 new=[]
 for post in data["data"]["children"]:
   post = post['data']
   if not post["id"] in seen:
+    print "New post", post
     writer.write(post["title"]+"\n")
     if post["domain"] == "self.%s" % REDDIT:
       writer.write(post["url"]+"\n")
     else:
-      writer.write(post["url"]+" "+post["permalink"]+"\n")
+      writer.write(post["url"]+" https://reddit.com"+post["permalink"]+"\n")
     new.append(post["id"])
 if len(new) != 0:
   f = open(STATEFILE, "a")
